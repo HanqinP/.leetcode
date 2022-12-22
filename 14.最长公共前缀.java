9330b413b8901299260586bf7e62ad1a30150f8b
彭涵骎 <penghq@inutan.com>
2022-12-22 18:48:20 +0800

    221222-add test class

--- 14.最长公共前缀.java
+++ 14.最长公共前缀.java
@@ -7,10 +7,30 @@
 // @lc code=start
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
+        String result = strs[0];
 
-        return null;
+        if(strs.length==0) return "";
+
+        if(strs.length==1) return strs[0];
+
+        for(int i = 1; i<strs.length;i++){
+            for(int j = 0;j<strs[i].length();j++){
+                if(result.charAt(j) != strs[i].charAt(j) && j == 0){
+                    return "";
+                }else if(!result.contains(strs[i].substring(0, j+1))){
+                    result = strs[i].substring(0,j);
+                    break;
+                }
+
+                if(strs[i].length()==1) result = strs[i];
+
+            }
+        }
+        return result;
     }
 }
+
+
+
 // @lc code=end
 
